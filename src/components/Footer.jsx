eca917ce498b6a37627dab6ae5c25e91c88c605b
rishn <aryanrishaan@gmail.com>
2026-04-03 19:47:05 +0530

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-left">v2.6.4</div>
-      <div className="footer-content">Matte by Educify™ An EduTech Enterprise 2025</div>
+      <div className="footer-content">Matte by Educify™ An EduTech Enterprise 2026</div>
     </footer>
   )
 }
